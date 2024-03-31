cf7b68bb9d4d52c2324400bdeedc03f290f2b22a
Milene Fukuda <milene.fukuda@gmail.com>
2024-03-31 22:07:48 +0100

    read me file

--- sync.py
+++ sync.py
@@ -5,21 +5,10 @@ import time
 import logging
 import sys
 
-# .1 criar teste unitário para função hash_file
-# .2 quebrar a funçao synchronize_folders em mais funçoes e escrever testes para elas
-# .3 refatorar código para que source_folder, replica_folder, sync_interval_seconds e log_file
-#    sejam obrigatoriamente informados como argumento no comando, caso contrario, exiba msg de "Usage". 
-#    Ex.: python sync.py source replica 1 replica.log
-# .4 adicionar logs ao console output
-
 def setup_logger(log_file):
-    print(f"alooo")
     # Create logger
     logger = logging.getLogger(__name__)
     logging.basicConfig(filename=log_file, encoding='utf-8', level=logging.DEBUG)
-
-    # return logger
-
     logger.setLevel(logging.DEBUG)
 
     # # Create console handler and set level to INFO
@@ -41,43 +30,45 @@ def setup_logger(log_file):
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
-print(f"aqui")
-logger = setup_logger("replica.log")
-print(f"ou aqui")
+    return logger
 
-#source_folder = ""
-#replica_folder = ""
-#argv = len(sys.argv)
+def hash_file(file_path):
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as file:
+        while True:
+            data = file.read(65536)  # 64 KB chunks
+            if not data:
+                break
+        #    print(file_path, data)
+            hasher.update(data)
+    return hasher.hexdigest()
 
-def check_folders_exists(source_folder, replica_folder):
-    print(f"parei aqui 00", replica_folder, source_folder)
-    if os.path.exists(source_folder) and os.path.exists(replica_folder):
-        return True
-    else:
-        return False
+def check_arguments(args):
+    if len(args) != 4:
+        print(f"Error: to sync please run: python3 sync.py <source folder> <replica folder> <interval in seconds>.")
+        exit()
 
-def synchronize_folders(source_folder, replica_folder):
-    print(f"parei aqui 01")
-    print(check_folders_exists(source_folder, replica_folder))
-    if False == check_folders_exists(source_folder, replica_folder):
-        raise Exception("Source or Replica folder's missing, check given arguments") 
-    # Get list of files in source folder
-    source_files = os.listdir(source_folder)
-    
-    for file_name in source_files:
-        source_path = os.path.join(source_folder, file_name)
-        replica_path = os.path.join(replica_folder, file_name)
+    # verifica se a pasta source existe, caso contrário da erro e sai do sistema
+    if check_folder_exists(args[1]) == False:
+        print(f"Error: Invalid source folder.")
+        exit()
 
-        if os.path.isdir(source_path):
-            # Recursively synchronize subfolders
-            synchronize_folders(source_path, replica_path)
-        else:
-            # Copy file if it doesn't exist in replica folder or if it's different
-            if not os.path.exists(replica_path) or hash_file(source_path) != hash_file(replica_path):
-                shutil.copy2(source_path, replica_path)
-                logger.info(f"Copied {file_name} from '{source_folder}' to '{replica_folder}'")
+    # verifica se a pasta replica existe, caso contrário da erro e sai do sistema
+    if check_folder_exists(args[2]) == False:
+        print(f"Error: Invalid replica folder.")
+        exit()
 
-def delete_replica_files(source_folder, replica_folder):
+    # verifica se o numero interval foi informado, caso contrário da erro e sai do sistema
+    if not args[3].isdigit():
+        print(f"Error: Invalid interval number.")
+        exit()
+    # caso nenhuma das condicoes anteriores seja atendida, nao faz mais nada, 
+    # apenas devolve a execucao para a funcao anterior
+
+def check_folder_exists(folder):
+    return os.path.exists(folder)
+
+def delete_replica_files(source_folder, replica_folder, logger, modified=0):
     # Remove files from replica that don't exist in source
     replica_files = os.listdir(replica_folder)
     for file_name in replica_files:
@@ -91,28 +82,51 @@ def delete_replica_files(source_folder, replica_folder):
             else:
                 os.remove(replica_path)
                 logger.info(f"Removed file {file_name} from '{replica_folder}'")
+                modified += 1
 
-def hash_file(file_path):
-    hasher = hashlib.md5()
-    with open(file_path, 'rb') as file:
-        while True:
-            data = file.read(65536)  # 64 KB chunks
-            if not data:
-                break
-        #    print(file_path, data)
-            hasher.update(data)
-    return hasher.hexdigest()
+    return modified
 
-# Example usage:
-source_folder = "source"
-replica_folder = "replica"
-argv = len(sys.argv)
-sync_interval_seconds = 1
+def synchronize_folders(source_folder, replica_folder, logger, modified=0):
+    # Ensure replica folder exists
+    if not os.path.exists(replica_folder):
+        os.makedirs(replica_folder)
+
+    # Get list of files in source folder
+    source_files = os.listdir(source_folder)
+    
+    for file_name in source_files:
+        source_path = os.path.join(source_folder, file_name)
+        replica_path = os.path.join(replica_folder, file_name)
+
+        if os.path.isdir(source_path):
+            # Recursively synchronize subfolders
+            synchronize_folders(source_path, replica_path, logger, modified)
+        else:
+            # Copy file if it doesn't exist in replica folder or if it's different
+            if not os.path.exists(replica_path) or hash_file(source_path) != hash_file(replica_path):
+                shutil.copy2(source_path, replica_path)
+                logger.info(f"Copied {file_name} from '{source_folder}' to '{replica_folder}'")
+                modified += 1
+    
+    # Remove files from replica that don't exist in source
+    modified = delete_replica_files(source_folder, replica_folder, logger, modified)
+
+    return modified
 
 def run():
+    check_arguments(sys.argv)
+    source_folder = sys.argv[1]
+    replica_folder = sys.argv[2]
+    sync_interval_seconds = int(sys.argv[3])
+    logger = setup_logger("sync.log")
+    
     while True :
-        synchronize_folders(source_folder, replica_folder)
-        print("cheguei até aqui", sys.argv[0])
+        total_files = synchronize_folders(source_folder, replica_folder, logger)
+
+        if total_files == 0:
+            logger.info(f"Nothing to update.")
+        else:
+            logger.info(f"All files are updated.")
         time.sleep(sync_interval_seconds)
 
 run()
--- test_sync.py
+++ test_sync.py
@@ -1,9 +1,9 @@
 import unittest
 from sync import hash_file
-from sync import check_replica_folder_exists
+from sync import synchronize_folders
 from unittest.mock import patch, mock_open
 
-class TestHashFile(unittest.TestCase):
+class TestSync(unittest.TestCase):
 
     def test_hash_file(self):
         temp_file_content  = b'test veeam content'
@@ -16,16 +16,14 @@ class TestHashFile(unittest.TestCase):
             expected_hash = '8d4718497ac6a3ab25a81238bfdc024b'
 
             self.assertEqual(calculated_hash, expected_hash)
-
-class TestCheckReplicaFolder(unittest.TestCase):
-
-    def test_check_replica_folder(self):
+    
+    def test_synchronize_folders(self):
         test_source_folder = "random content"
         test_replica_folder = "random content"
 
         with patch("sync.open", mock_open(read_data=test_source_folder)) as mock_file:
             print("qqqq")
-            check_replica_folder_exists(test_replica_folder)
+            synchronize_folders(test_replica_folder)
             self.assertEqual(test_source_folder, test_replica_folder)
 
 if __name__=='__main__':
